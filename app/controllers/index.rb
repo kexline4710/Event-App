534d682fb7e5ad94de68d6d63f7389cc60183804
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-15 12:19:04 -0600

    added basic styling

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,13 @@
 get '/' do
   # Look in app/views/index.erb
   @events = Event.all
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+    @created = @user.created_events
+    @attended = @user.attended_events
+    hash = Digest::MD5.hexdigest(@user.email.downcase)
+    @image = "http://www.gravatar.com/avatar/#{hash}"
+  end
   erb :index
 end
 
